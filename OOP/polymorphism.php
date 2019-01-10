186cbfd0c5ce5f4ce05ee080092f43ef79cb0c7e
Tarek <tarekcse0142@gmail.com>
2019-01-10 16:51:11 +0600

    inheritance and polymorphism

---
+++ OOP/polymorphism.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Polymorphism
+ */
+class UserData{
+	public $user;
+	public $userid;
+	function __construct($userName, $userId)
+	{
+		$this->user = $userName;
+		$this->userid = $userId;
+	}
+	public function display(){
+        echo "User name is {$this->user} and User id is {$this->userid}";
+	}
+}
+
+class Admin extends UserData
+{
+	public $level;
+
+	public function display(){
+        echo "User name is {$this->user} and User id is {$this->userid} and user level is {$this->level}";
+	}
+}
+$user = "Mamun";
+$id = "25";
+$userData = new UserData($user,$id);
+$userData->display();
+echo "<br>";
+$user = "Admin";
+$id ="1";
+$admin = new Admin($user,$id);
+$admin->level = "Administor";
+$admin->display();
+echo "<br>";
+if ($admin instanceof UserData) {
+	echo "Inherited";
+}
+?>
